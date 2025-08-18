41d1912915e4244767d32cb03a2f4d2177d4819d
Timur Kasimov <timakasimovktr@gmail.com>
2025-08-18 09:22:13 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,66 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Montserrat, Playfair_Display } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import Script from "next/script";
+import type { Metadata } from "next";
+import { Montserrat, Playfair_Display } from "next/font/google";
+import "./globals.css";
 
 const montserrat = Montserrat({
   subsets: ["latin", "cyrillic"],
   display: "swap",
   variable: "--font-montserrat",
   weight: ["400", "500", "600", "700"],
-})
+});
 
 const playfair = Playfair_Display({
   subsets: ["latin", "cyrillic"],
   display: "swap",
   variable: "--font-playfair",
-})
+});
 
 export const metadata: Metadata = {
   title: "Юридические и бухгалтерские услуги в Узбекистане",
-  description: "Комплексные решения для регистрации, сопровождения и защиты интересов физических и юридических лиц",
+  description:
+    "Комплексные решения для регистрации, сопровождения и защиты интересов физических и юридических лиц",
   generator: "v0.app",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="ru" className={`${montserrat.variable} ${playfair.variable} antialiased`}>
-      <body>{children}</body>
+    <html
+      lang="ru"
+      className={`${montserrat.variable} ${playfair.variable} antialiased`}
+    >
+      <body>
+        <Script id="facebook-pixel-thankyou" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1416152896331309');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1416152896331309&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
